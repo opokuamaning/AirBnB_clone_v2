133ac5c9cbc289551c2930922f612b5366f89b91
Prince Opoku Amaning <princoprince94@gmail.com>
2024-05-20 12:02:54 +0000

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -10,10 +10,10 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def home():
-    """ Display Hello HBNB! """
-    return 'Hello HBNB!'
+def hello_hbnb():
+    """display text"""
+    return "Hello HBNB!"
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
