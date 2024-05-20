5ece0fec11becd442529c1b539852d7c992d96f6
Prince Opoku Amaning <princoprince94@gmail.com>
2024-05-20 11:59:14 +0000

    added 0-hello_route.p

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    Start a Flask web application
+    lisen on 0.o.o.o, port 5000
+    route (/) display "Hello HBNB!
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """ Display Hello HBNB! """
+    return 'Hello HBNB!'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
